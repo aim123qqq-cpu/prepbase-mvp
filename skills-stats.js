4bdefbe5a9844903e1a33cdc6d07f2b247e36d25
aim123qqq-cpu <aim123qqq@gmail.com>
2026-05-14 14:29:04 +0300

    Add empty Telegram skills stats

---
+++ skills-stats.js
@@ -0,0 +1,13 @@
+window.PREPBASE_SKILL_STATS = {
+  updatedAt: null,
+  sources: [
+    "https://t.me/saba_hunter",
+    "https://t.me/analysts_hunter",
+    "https://t.me/analyst_job",
+    "https://t.me/tzprofi_job"
+  ],
+  totalMessages: 0,
+  totalVacancies: 0,
+  skills: [],
+  errors: []
+};
